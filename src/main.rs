4b3ce739026e7193278e9dbd340386e6b023489a
Zach <zwc101@gmail.com>
2019-08-24 20:16:51 -0700

    added middleware

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,115 @@
 extern crate gotham;
+#[macro_use]
+extern crate gotham_derive;
 extern crate futures;
 extern crate mime;
-
-#[macro_use]
-extern crate lazy_static;
+extern crate serde;
+extern crate serde_json;
 
 use futures::future;
 
 use gotham::helpers::http::response::create_response;
 use gotham::handler::HandlerFuture;
 use gotham::router::builder::*;
-use gotham::router::Router;
-use gotham::state::State;
+use gotham::state::{FromState, State};
+use gotham::middleware::state::StateMiddleware;
+use gotham::pipeline::single_middleware;
+use gotham::pipeline::single::single_pipeline;
 
 use hyper::StatusCode;
 
+use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 
-lazy_static! {
-    static ref SECRETS: HashMap<&'static str, &'static str> = {
-        let mut map = HashMap::new();
-        map.insert("test", "hello");
-        map
+use rusoto_core::Region;
+use rusoto_secretsmanager::{GetSecretValueRequest, ListSecretsRequest, SecretsManager, SecretsManagerClient};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Secret{
+    #[serde(rename="Test")]
+    test: String,
+}
+
+#[derive(Clone, StateData)]
+struct Substr8 {
+    secrets: HashMap<String, String>,
+   // pub client: SecretsManagerClient,
+}
+
+impl Substr8 {
+    fn new() -> Self {
+
+
+        // Build client for later
+        let client = SecretsManagerClient::new(Region::UsWest2);
+
+        // Build Internal Mapping
+        let mut secrets = HashMap::new();
+        let secret_list = list_secrets(&client);
+        for secret in secret_list.iter() {
+            for s in secret {
+                println!("{}", s.name
+                                .clone()
+                                .unwrap());
+                let val = get_secret(&client, s.name
+                                                .clone()
+                                                .unwrap());
+                secrets.insert(s.name
+                                .clone()
+                                .unwrap(), val); // Handle errors like an adult
+            }
+        }
+
+        Self {secrets}
+    }
+}
+
+// Retrieves an individual secret given a name
+fn get_secret(client: &SecretsManagerClient, name: String) -> String {
+    let mut req = GetSecretValueRequest::default();
+    req.secret_id = name;
+    let res = client.get_secret_value(req)
+                        .sync();
+
+    let secret = res.unwrap().secret_string.unwrap();
+    println!("{:?}", secret);
+
+    let pair: Secret = serde_json::from_str(&secret)
+                            .expect("Couldn't deserialize");
+    pair.test
+}
+
+// Retrieves all secrets in the Secret Manager
+fn list_secrets(client: &SecretsManagerClient) -> Option<std::vec::Vec<rusoto_secretsmanager::SecretListEntry>> {
+    let req = ListSecretsRequest{
+        ..Default::default()
     };
+    let result = client.list_secrets(req).sync();
+    result.unwrap().secret_list
 }
 
 fn main() {
+
+    let middleware = StateMiddleware::new(Substr8::new());
+    let pipeline = single_middleware(middleware);
+
+    let (chain, pipelines) = single_pipeline(pipeline);
     let addr = "0.0.0.0:7878";
     println!("Listening for requests at http://{}", addr);
-    gotham::start(addr, router());
+    gotham::start(addr, build_router(chain, pipelines, |route| {
+        route.get("/secrets").to(secrets_handler);
+    }));
 }
 
-fn hello_world(state: State) -> Box<HandlerFuture>{
-    let f = "Hello World";
-    println!("{}", SECRETS["test"]);
+
+fn secrets_handler(state: State) -> Box<HandlerFuture>{
+    let sub = Substr8::borrow_from(&state);
+    let secret = sub.secrets["SecretTest"].clone();
     let response = create_response(
                     &state, 
                     StatusCode::OK,
                     mime::TEXT_JAVASCRIPT,
-                    f
+                    secret
                     );
     Box::new(future::ok((state, response)))
-}
-
-//Build our Gotham router
-pub fn router() -> Router {
-    build_simple_router(
-    |route| {
-        route.associate("/secret", 
-        |assoc| { 
-            assoc.post().to(hello_world);
-        });
-    })
 }
